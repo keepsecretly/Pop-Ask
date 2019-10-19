cf472d82a3317f604ccf7665811225734cfa101c
NiM NiM <topsecret808@gmail.com>
2019-10-19 17:53:22 +0700

    :lipstick: Render page

--- lib/api_demo.dart
+++ lib/api_demo.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:pop_ask/api/survey_api.dart';
+import 'package:pop_ask/model/page_model.dart';
+import 'package:pop_ask/model/question_model.dart';
 import 'package:pop_ask/model/surveys_model.dart';
+import 'package:pop_ask/question_list.dart';
 
 class ApiDemo extends StatefulWidget {
   ApiDemo({Key key}) : super(key: key);
@@ -11,6 +14,8 @@ class ApiDemo extends StatefulWidget {
 
 class _ApiDemoState extends State<ApiDemo> {
   Future<Surveys> surveys;
+
+  List<Page> pages = [];
 
   @override
   void initState() {
@@ -34,7 +39,8 @@ class _ApiDemoState extends State<ApiDemo> {
             future: surveys,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
-                return Text(snapshot.data.surveys[0].title);
+//                return Text(snapshot.data.surveys[0].title);
+                return firstPage(context);
               } else if (snapshot.hasError) {
                 return Text("${snapshot.error}");
               }
@@ -45,5 +51,13 @@ class _ApiDemoState extends State<ApiDemo> {
         ),
       ),
     );
+  }
+
+  Widget firstPage(BuildContext context) {
+      if (pages.length > 0) {
+        return new ListDisplay(quizes: pages[0].questions,);
+      } else {
+        return Text("This page has no question");
+      }
   }
 }
---
+++ lib/question_list.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/material.dart';
+import 'package:pop_ask/model/question_model.dart';
+
+final TextEditingController eCtrl = new TextEditingController();
+
+class ListDisplay extends StatefulWidget {
+  final List<Question> quizes;
+
+  // Declare a field that holds the Todo.
+//  final Todo todo;
+
+  // In the constructor, require a Todo.
+  ListDisplay({Key key, @required this.quizes}) : super(key: key);
+
+  @override
+  State createState() => new DyanmicList();
+
+}
+
+class DyanmicList extends State<ListDisplay> {
+  List<String> litems = [];
+
+//  List<Question> quizes = [
+//    Question(),
+//    Question(),
+//    Question(),
+//    Question(),
+//    Question(),
+//  ];
+
+  List<String> quizes = [];
+//    "Short 1",
+//    "Short 2",
+//    "Long 1",
+//    "Long 2",
+//  ];
+
+  final _formKey = GlobalKey<FormState>();
+
+  Widget buildQuestionList (BuildContext ctxt) {
+    return Form(
+        key: _formKey,
+        child: ListView(
+            padding: const EdgeInsets.all(8),
+            children: <Widget>[
+              for(var q in quizes ) buildAnswer(ctxt, q),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: RaisedButton(
+                  onPressed: () {
+                    // Validate returns true if the form is valid, or false
+                    // otherwise.
+                    if (_formKey.currentState.validate()) {
+                      Scaffold.of(ctxt)
+                          .showSnackBar(SnackBar(content: Text('Processing Data')));
+                    }
+                  },
+                  child: Text('Submit'),
+                ),
+              ),
+            ]
+        )
+    );
+  }
+
+  Widget buildAnswer (BuildContext ctxt, String qtype) {
+      if (qtype.matchAsPrefix("Long") == true) {
+        return buildLongAnswer(ctxt);
+      } else if (qtype.matchAsPrefix("Short") == true) {
+        return buildShortAnswer(ctxt);
+      } else {
+        return new Text("");
+      }
+  }
+
+  Widget buildShortAnswer (BuildContext ctxt) {
+
+    final TextEditingController eCtrl = new TextEditingController();
+
+    return new Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        child: Column(
+          children: <Widget>[
+            Text("Question"),
+            TextFormField(
+              controller: eCtrl,
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+              onFieldSubmitted: (text) {
+                litems.add(text);
+                eCtrl.clear();
+                setState(() {});
+              },
+            )
+          ],
+        )
+    );
+  }
+
+  Widget buildLongAnswer (BuildContext ctxt) {
+
+    final TextEditingController eCtrl = new TextEditingController();
+
+    return new Padding(
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        child: Column(
+          children: <Widget>[
+            Text("Question"),
+            TextFormField(
+              keyboardType: TextInputType.multiline,
+              maxLines: 5,
+              controller: eCtrl,
+              validator: (value) {
+                if (value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+              onFieldSubmitted: (text) {
+                litems.add(text);
+                eCtrl.clear();
+                setState(() {});
+              },
+            )
+          ],
+        )
+    );
+  }
+
+  @override
+  Widget build (BuildContext ctxt) {
+    return new Scaffold(
+      body: buildQuestionList(ctxt),
+    );
+  }
+}
